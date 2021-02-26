3a38e7bb2f5f6d73a2af4a5a984850625735b705
Bikash Sapkota <sapkotabikash194@gmail.com>
2021-02-26 18:22:20 +0545

    Update configuration.go

--- configuration.go
+++ configuration.go
@@ -3,38 +3,40 @@ package go_db
 import (
 	"errors"
 	"os"
-	//"fmt"
+	"fmt"
 )
 
 
 func ConnString() (connStr string, err error){
+	
 	if os.Getenv("DB_USER") == "" {
 		return connStr, errors.New("Empty user.")
 	}
-	//student:student@/musicapp?charset=utf8&parseTime=True
-	connStr += os.Getenv("DB_USER") + ":"
+	
 
 
 	if os.Getenv("DB_PASSWORD") == "" {
 		return connStr, errors.New("Empty password.")
 	}
-	connStr += os.Getenv("DB_PASSWORD") + "@/"
+	
 
 
 	if os.Getenv("DB_HOST") == "" {
 		return connStr, errors.New("Empty hostname.")
 	}
-	//connStr += " host=" + os.Getenv("DB_HOST")
+	
 	if os.Getenv("DB_PORT") == "" {
 		return connStr, errors.New("Empty port number.")
 	}
-	//connStr += " port=" + os.Getenv("DB_PORT")
+	
 
 	if os.Getenv("DB_NAME") == "" {
 		return connStr, errors.New("Empty database name.")
 	}
-	connStr += os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True"
-	//connStr += " sslmode=" + os.Getenv("SSL_MODE")
+	
 
+	connStr = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", 
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),os.Getenv("DB_NAME"))
+	
 	return connStr, err
 }
