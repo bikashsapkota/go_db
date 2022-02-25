018c1459e9ea9759257719e6864bcb7aba29e659
bikashsapkota <sapkotabikash194@gmail.com>
2022-02-25 21:20:08 +0545

    code refactor and GetDjUserIdWithGenre

--- model/model.go
+++ model/model.go
@@ -4,52 +4,52 @@ import (
 	"time"
 )
 
-
 type IdentifiedMusics struct {
-  Id int
-  MusicId int
-  DjId int
-  PlayedTimestamp time.Time
-  CreatedAt time.Time
-  UpdatedAt time.Time
-  videos string
+	Id              int
+	MusicId         int
+	DjId            int
+	PlayedTimestamp time.Time
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	videos          string
 }
 
 type MusicCampaign struct {
-  Id int
-  UserId int
-  CampaignName string
-  Email string
-  SpinRate int
-  CampaignBalance int
-  CreatedAt time.Time
-  UpdatedAt time.Time
+	Id              int
+	UserId          int
+	CampaignName    string
+	Email           string
+	SpinRate        int
+	CampaignBalance int
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 }
 
 type Notifications struct {
-  Id int
-  UserId int
-  Message string
-  Href string
-  Seen bool
-  Type string
-  CreatedAt time.Time
-  UpdatedAt time.Time
-  ReferenceId int
-  Subject string
-  Image string
+	Id          int
+	UserId      int
+	Message     string
+	Href        string
+	Seen        bool
+	Type        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	ReferenceId int
+	Subject     string
+	Image       string
+	IsShown     int `default:"0"`
 }
 
-type KafkaMessages  struct {
-  ID int
-  Topic string
-  Message string
+type KafkaMessages struct {
+	ID      int
+	Topic   string
+	Message string
 }
 
 type Job struct {
- Id int
- Title string
- Interval int
- Unit string
- StartTime time.Time
+	Id        int
+	Title     string
+	Interval  int
+	Unit      string
+	StartTime time.Time
 }
